9f56eddf7cec6596d8b43739ae2a174d4de6b7fe
b4zeng <b4zeng@student.cs.uwaterloo.ca>
2020-09-26 12:37:09 -0400

    locks

--- os161-1.99/kern/thread/synch.c
+++ os161-1.99/kern/thread/synch.c
@@ -163,8 +163,17 @@ lock_create(const char *name)
                 return NULL;
         }
         
-        // add stuff here as needed
+        lock->lk_wchan = wchan_create(lock->lk_name);
+	if (lock->lk_wchan == NULL) {
+		kfree(lock->lk_name);
+		kfree(lock);
+		return NULL;
+	}
         
+        spinlock_init(&lk->lk_lock);
+        lock->held = false;
+        lock->lk_owner = NULL;
+
         return lock;
 }
 
